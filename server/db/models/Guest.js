7db498671e55c155cba839b80ea1240fb5822db7
Yehoon Joo <lukejoo1092@gmail.com>
2022-01-11 16:03:46 -0500

    refactor(database index) require new models to index.js file
    Require User, Guest, Product to index.js in db folder

--- server/db/models/Guest.js
+++ server/db/models/Guest.js
@@ -30,56 +30,56 @@ const Guest = db.define('guest', { // columns set up prior to guest checkout, wh
 
 module.exports = Guest
 
-/**
- * instanceMethods
- */
-Guest.prototype.correctPassword = function(candidatePwd) {
-  //we need to compare the plain version to an encrypted version of the password
-  return bcrypt.compare(candidatePwd, this.password);
-}
+// /**
+//  * instanceMethods
+//  */
+// Guest.prototype.correctPassword = function(candidatePwd) {
+//   //we need to compare the plain version to an encrypted version of the password
+//   return bcrypt.compare(candidatePwd, this.password);
+// }
 
-Guest.prototype.generateToken = function() {
-  return jwt.sign({id: this.id}, process.env.JWT)
-}
+// Guest.prototype.generateToken = function() {
+//   return jwt.sign({id: this.id}, process.env.JWT)
+// }
 
-/**
- * classMethods
- */
-Guest.authenticate = async function({ username, password }){
-    const user = await this.findOne({where: { username }})
-    if (!user || !(await user.correctPassword(password))) {
-      const error = Error('Incorrect username/password');
-      error.status = 401;
-      throw error;
-    }
-    return user.generateToken();
-};
+// /**
+//  * classMethods
+//  */
+// Guest.authenticate = async function({ username, password }){
+//     const user = await this.findOne({where: { username }})
+//     if (!user || !(await user.correctPassword(password))) {
+//       const error = Error('Incorrect username/password');
+//       error.status = 401;
+//       throw error;
+//     }
+//     return user.generateToken();
+// };
 
-Guest.findByToken = async function(token) {
-  try {
-    const {id} = await jwt.verify(token, process.env.JWT)
-    const user = Guest.findByPk(id)
-    if (!user) {
-      throw 'nooo'
-    }
-    return user
-  } catch (ex) {
-    const error = Error('bad token')
-    error.status = 401
-    throw error
-  }
-}
+// Guest.findByToken = async function(token) {
+//   try {
+//     const {id} = await jwt.verify(token, process.env.JWT)
+//     const user = Guest.findByPk(id)
+//     if (!user) {
+//       throw 'nooo'
+//     }
+//     return user
+//   } catch (ex) {
+//     const error = Error('bad token')
+//     error.status = 401
+//     throw error
+//   }
+// }
 
-/**
- * hooks
- */
-const hashPassword = async(user) => {
-  //in case the password has been changed, we want to encrypt it with bcrypt
-  if (user.changed('password')) {
-    user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
-  }
-}
+// /**
+//  * hooks
+//  */
+// const hashPassword = async(user) => {
+//   //in case the password has been changed, we want to encrypt it with bcrypt
+//   if (user.changed('password')) {
+//     user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+//   }
+// }
 
-Guest.beforeCreate(hashPassword)
-Guest.beforeUpdate(hashPassword)
-Guest.beforeBulkCreate(users => Promise.all(users.map(hashPassword)))
+// Guest.beforeCreate(hashPassword)
+// Guest.beforeUpdate(hashPassword)
+// Guest.beforeBulkCreate(users => Promise.all(users.map(hashPassword)))
