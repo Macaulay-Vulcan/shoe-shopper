67ce78348337d503f446441c892b74e83feccbdd
Blaine Interrante <interrante.blaine@gmail.com>
2022-01-11 15:32:56 -0500

    feat(NavBar): adds links on nav to products, account, and cart

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -15,15 +15,21 @@ const Navbar = () => {
           <div>
             {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
+            <Link to="/home">Products</Link>
+            <Link to="/home">Account</Link>
             <a href="#" onClick={() => dispatch(logout())}>
               Logout
             </a>
+            <Link to="/home">Cart</Link>
           </div>
         ) : (
           <div>
             {/* The navbar will show these links before you log in */}
+            <Link to="/home">Home</Link>
+            <Link to="/home">Products</Link>
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            <Link to="/home">Cart</Link>
           </div>
         )}
       </nav>
