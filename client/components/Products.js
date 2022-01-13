5dc0bbb47d5b86cee978a846c7aefbdee67edeff
Johan Perez <1johanperez@gmail.com>
2022-01-13 14:26:42 -0500

    create : create all product view
    All product view for Admin/Guests/Users is the same, no functionality involved
    fixes #40

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,31 +1,38 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { fetchProducts } from '../store/products'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchProducts } from '../store/products';
 
 const Products = () => {
-    const products = useSelector(state => state.products);
-    const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchProducts())
-    }, [])
+  useEffect(() => {
+    dispatch(fetchProducts());
+    setLoading(false);
+  }, []);
 
-    return (
-        <div>
-            {
-                products.map(prod => (
-                    <div key={prod.id}>
-                        <Link to={`/products/${prod.id}`}>
-                            <img src={prod.imageUrl} />
-                            <h3>{prod.description}</h3>
-                            <h4>{`$${prod.price}`}</h4>
-                        </Link>
-                    </div>
-                ))
-            }
+  if (loading) return <div>Loading</div>;
+  return (
+    <div>
+      <h1>PRODUCTS IN STOCK : </h1>
+      {products.map((prod) => (
+        <div key={prod.id}>
+          <h2>
+            <Link to={`/products/${prod.id}`}>
+              {' '}
+              {prod.name || 'Product Name not available yet!'}{' '}
+            </Link>
+          </h2>
+          <h3>{prod.brand}</h3>
+          <img src={prod.imageUrl} />
+          <h3>{prod.description}</h3>
+          <h4>{`$${prod.price}`}</h4>
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Products;
