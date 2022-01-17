c4ffb77d4965659228642ccc91ca90b662e70915
Yehoon Joo <lukejoo1092@gmail.com>
2022-01-17 16:19:32 -0500

    refactor(signup): display necessary fields for signup and validate unique email
    Display email and address as required fields for signup form and validates for unique username and email
    
    close #52

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,42 +1,64 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { authenticate } from '../store'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authenticate } from "../store";
 
-const AuthForm = ({ name, displayName}) => {
-  const { error } = useSelector(state => state.auth);
-  const dispatch = useDispatch();
+const AuthForm = ({ name, displayName }) => {
+	const { error } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formName = e.target.name;
-    const username = e.target.username.value;
-    const password = e.target.password.value;
-    dispatch(authenticate(username, password, formName));
-  }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formName = e.target.name;
+		const username = e.target.username.value;
+		const password = e.target.password.value;
+		if (formName === "login")
+			dispatch(authenticate(username, password, formName));
+		else {
+			const email = e.target.email.value;
+			const address = e.target.address.value;
+			dispatch(authenticate(username, password, formName, email, address));
+		}
+	};
 
-  return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="username">
-            <small>Username</small>
-          </label>
-          <input name="username" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
-    </div>
-  )
-}
+	return (
+		<div>
+			<form onSubmit={handleSubmit} name={name}>
+				<div>
+					<label htmlFor="username">
+						<small>Username</small>
+					</label>
+					<input name="username" type="text" />
+				</div>
+				{name === "signup" && (
+					<div>
+						<div>
+							<label htmlFor="email">
+								<small>Email</small>
+							</label>
+							<input name="email" type="text" />
+						</div>
+						<div>
+							<label htmlFor="address">
+								<small>Address</small>
+							</label>
+							<input name="address" type="text" />
+						</div>
+					</div>
+				)}
+				<div>
+					<label htmlFor="password">
+						<small>Password</small>
+					</label>
+					<input name="password" type="password" />
+				</div>
+				<div>
+					<button type="submit">{displayName}</button>
+				</div>
+				{error && error.response && <div> {error.response.data} </div>}
+			</form>
+		</div>
+	);
+};
 
-export const Login = <AuthForm name="login" displayName="Login" />
-export const Signup = <AuthForm name="signup" displayName="Sign Up" />
+export const Login = <AuthForm name="login" displayName="Login" />;
+export const Signup = <AuthForm name="signup" displayName="Sign Up" />;
